649a5e71dadc347715cd028fa3f10c588c1a953d
Michel Bernardo <michel_13s@outlook.com>
2023-09-29 18:53:59 -0300

    test: validate test with invalid position

--- src/test/java/com/navegation/robot/NavegationRoboApplicationTests.java
+++ src/test/java/com/navegation/robot/NavegationRoboApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -63,7 +62,7 @@ public class NavegationRoboApplicationTests {
         ResponseEntity<Object> responseEntity = roboController.sendCommand(command);
 
         assertEquals(0, robo.getX());
-        assertEquals(4, robo.getY());
+        assertEquals(24, robo.getY());
         assertEquals('N', robo.getDirection());
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
         assertEquals("Invalid Position", responseEntity.getBody());
