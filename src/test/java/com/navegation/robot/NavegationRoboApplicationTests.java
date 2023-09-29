3a64838a617c3f59a98b812ed0bbcc85e9153b7b
Michel Bernardo <michel_13s@outlook.com>
2023-09-29 16:43:17 -0300

    test: adding test in application

--- src/test/java/com/navegation/robot/NavegationRoboApplicationTests.java
+++ src/test/java/com/navegation/robot/NavegationRoboApplicationTests.java
@@ -1,10 +1,14 @@
 package com.navegation.robot;
 
+import com.navegation.robot.controller.RoboController;
 import com.navegation.robot.model.Robo;
 import com.navegation.robot.service.RoboService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,21 +17,56 @@ public class NavegationRoboApplicationTests {
 
     @Autowired
     private RoboService roboService;
+    @Autowired
+    private RoboController roboController;
 
     @Test
     public void testSendCommands_MML() {
-        Robo robo = roboService.sendCommands("MML");
+        String command = "MML";
+
+        Robo robo = roboService.sendCommands(command);
+        ResponseEntity<Object> responseEntity = roboController.sendCommand(command);
+
         assertEquals(0, robo.getX());
         assertEquals(2, robo.getY());
         assertEquals('W', robo.getDirection());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
 
     @Test
     public void testSendCommands_MRMRMLM() {
-        Robo robo = roboService.sendCommands("MRMRMLM");
+        String command = "MMRMMRMM";
+
+        Robo robo = roboService.sendCommands(command);
+        ResponseEntity<Object> responseEntity = roboController.sendCommand(command);
+
         assertEquals(2, robo.getX());
         assertEquals(0, robo.getY());
-        assertEquals('E', robo.getDirection());
+        assertEquals('S', robo.getDirection());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    void testSendCommands_AAA() {
+        String command = "AAA";
+        ResponseEntity<Object> responseEntity = roboController.sendCommand(command);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("Invalid Command", responseEntity.getBody());
+
+    }
+    @Test
+    void testSendCommands_MMMMMMMMMMMM() {
+        String command = "MMMMMMMMMMMMMMMMMMMMMMMM";
+        Robo robo = roboService.sendCommands(command);
+        ResponseEntity<Object> responseEntity = roboController.sendCommand(command);
+
+        assertEquals(0, robo.getX());
+        assertEquals(4, robo.getY());
+        assertEquals('N', robo.getDirection());
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("Invalid Position", responseEntity.getBody());
     }
 
 }
